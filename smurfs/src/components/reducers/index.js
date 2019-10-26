98b0b8dc188ebc08df59dcb788d9beba5b100325
Clif <clifhodges13@gmail.com>
2019-10-26 11:18:49 -0500

    initial commit. set up redux

---
+++ smurfs/src/components/reducers/index.js
@@ -0,0 +1,15 @@
+const initalState = [
+  {
+    "name": "Dopey",
+    "age": 3000,
+    "height": "3cm",
+    "id": 14
+  }
+]
+
+export function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
